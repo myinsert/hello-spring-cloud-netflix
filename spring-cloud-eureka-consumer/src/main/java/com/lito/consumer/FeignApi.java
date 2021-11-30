38fd1db652d733f545d272a3d0e56bd15b0423a4
myinsert <myinsert@163.com>
2021-11-30 15:10:14 +0800

    spring cloud netflix

--- spring-cloud-eureka-consumer/src/main/java/com/lito/consumer/FeignApi.java
+++ spring-cloud-eureka-consumer/src/main/java/com/lito/consumer/FeignApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @LoginName: ZDGG
  * @Date: 2021-11-29 20:09 星期一
  */
-@FeignClient(value = "hello-spring-cloud-discovery-02",fallback = FeignApiFallBack.class)
+@FeignClient(value = "hello-spring-cloud-producer",fallback = FeignApiFallBack.class)
 public interface FeignApi {
 
     @RequestMapping("/sayHi")
