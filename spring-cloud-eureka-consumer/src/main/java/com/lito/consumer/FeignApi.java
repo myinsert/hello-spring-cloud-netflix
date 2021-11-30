17d4eb621bbecdf41ee2ef7d18673b241ea9c098
myinsert <myinsert@163.com>
2021-11-30 15:07:52 +0800

    spring cloud netflix

---
+++ spring-cloud-eureka-consumer/src/main/java/com/lito/consumer/FeignApi.java
@@ -0,0 +1,18 @@
+package com.lito.consumer;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Auther: yoult
+ * @Description: TODO
+ * @LoginName: ZDGG
+ * @Date: 2021-11-29 20:09 星期一
+ */
+@FeignClient(value = "hello-spring-cloud-discovery-02",fallback = FeignApiFallBack.class)
+public interface FeignApi {
+
+    @RequestMapping("/sayHi")
+    String sayHi(@RequestParam("message") String message);
+}
---
+++ spring-cloud-eureka-producer02/src/main/java/com/lito/producer02/SpringCloudEurekaDiscovery02Application.java
@@ -0,0 +1,15 @@
+package com.lito.producer02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudEurekaDiscovery02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaDiscovery02Application.class, args);
+	}
+
+}
